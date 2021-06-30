e07e019412bd757b8668b69ea9d1a09c26badb25
ricardoapf <rp.antoniof@gmail.com>
2021-06-29 23:10:34 -0600

    ✨ add user routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const indexRouter = require('./routes/index');
 const pokemonsRouter = require('./routes/pokemons');
+const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/pokemons', pokemonsRouter);
 
 module.exports = app;
