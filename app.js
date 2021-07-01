e91a2c45482ca339a74ae56787c86926793799fd
ricardoapf <rp.antoniof@gmail.com>
2021-06-30 23:41:59 -0600

    🎨 update route and apply middlewares

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const indexRouter = require('./routes/index');
-const pokemonsRouter = require('./routes/pokemons');
-const usersRouter = require('./routes/users');
 const app = express();
+const indexRouter = require('./routes/index');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/pokemons', pokemonsRouter);
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const pokemonCtr = require('../controllers/pokemonController');
+const userCtr = require('../controllers/userController');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const authCtr = require('../controllers/authController');
+const env = require('../config/env');
+const middleware = passport.authenticate('jwt', { session: false });
+const isAdminMiddleware = authCtr.isAdmin;
 
 /* GET root. */
-router.get('/', function(req, res, next) {
-  res.send('Poke-api');
+router.get('/', (req, res, next) => {
+    res.send('Poke-api');
 });
+
+/* login. */
+router.post('/login', (req, res, next) => {
+    passport.authenticate('login', async (err, user, info) => {
+        try {
+            if (err || ! user) {
+                return res.status(401).send({
+                    message: 'credentials invalid'
+                });
+            }
+            req.login(user, { session: false }, async (err) => {
+                if (err) {
+                    return res.status(401).send({
+                        error: err,
+                        message: 'An error has occurred, please try again later'
+                    });
+                }
+                const token = jwt.sign({ user }, env.SECRET_TOKEN);
+                return res.send({ token });
+            });
+        } catch (e) {
+            return next(e);
+        }
+    })(req, res, next)
+});
+
+/* register.  */
+router.post('/register', authCtr.register);
+
+/* Middleware */
+router.use(middleware);
+
+/* Profile. */
+router.get('/profile', (req, res, next) => {
+    const user = {
+        'id': req.user.id,
+        'name': req.user.name,
+        'last_name': req.user.last_name,
+        'email': req.user.email,
+        'is_admin': req.user.is_admin
+    };
+    return res.send({ user });
+});
+
+/* pokemons routes. */
+router.get('/pokemons', pokemonCtr.index);
+router.post('/pokemons', isAdminMiddleware, pokemonCtr.store);
+router.get('/pokemons/:id', isAdminMiddleware, pokemonCtr.show);
+router.put('/pokemons/:id', isAdminMiddleware, pokemonCtr.update);
+router.delete('/pokemons/:id', isAdminMiddleware, pokemonCtr.delete);
+
+/* users routes. */
+router.get('/users', isAdminMiddleware, userCtr.index);
+router.post('/users', isAdminMiddleware, userCtr.store);
+router.get('/users/:id', isAdminMiddleware, userCtr.show);
+router.put('/users/:id', isAdminMiddleware, userCtr.update);
+router.delete('/users/:id', isAdminMiddleware, userCtr.delete);
 
 module.exports = router;
