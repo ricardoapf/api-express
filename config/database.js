8831ac764e1f1491326375b70b8e4503d8435433
ricardoapf <rp.antoniof@gmail.com>
2021-07-02 14:40:53 -0600

    🎨 remove necessaries values

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const env = require('../config/env');
 
 const con = mysql.createConnection({
-    host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || '',
-    database: env.DB_NAME || 'poke_api',
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
 });
 
 module.exports = con;
