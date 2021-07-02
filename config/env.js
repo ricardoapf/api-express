8b4d12af8e10a174642218f116d10023625928c9
ricardoapf <rp.antoniof@gmail.com>
2021-07-01 20:20:56 -0600

    🎨 add port db

--- config/env.js
+++ config/env.js
@@ -3,6 +3,7 @@ module.exports = {
     SECRET_TOKEN: process.env.SECRET_TOKEN || 'VO6CrykAAoFY0GIBqqswOdAkLAHQSqWwyGSohhn43mUUcdk3ujTuevA9U4p6bOZ4',
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_USER: process.env.DB_USER || 'root',
+    DB_PORT: process.env.DB_PORT || 3306,
     DB_PASSWORD: process.env.DB_PASSWORD || '',
     DB_NAME: process.env.DB_NAME || 'poke_api'
 
