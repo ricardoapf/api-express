32f8daf89dc8949c848d2db9446275718302a037
ricardoapf <rp.antoniof@gmail.com>
2021-07-02 10:06:25 -0600

    :memo: update documentation

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const indexRouter = require('./routes/index');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const options = require('./swagger.json');
+const cors = require('cors');
 
 const specs = swaggerJsDoc(options);
-console.log(specs);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
